fd818199fdc7c13e318dd902ac40546d9105f577
Diego <dgcastil@uc.cl>
2013-04-19 11:23:20 -0400

    se agrega general para procesos con solo tiempo de ejec

---
+++ General.py
@@ -0,0 +1,34 @@
+import time
+
+class General(Process):
+	
+	def enviar_ubicacion():
+
+		print "Enviando ubicación..."
+		time.sleep(otros[0]))
+		print "Ubicación enviada"
+
+	def ver_ubicacion():
+
+		print "Viendo ubicación..."
+		time.sleep(2)
+		print "Ubicación vista"
+
+	def jugar():
+
+		print "Jugando..."
+		time.sleep(otros[0]))
+		print "Realizado"
+
+	def escuchar_musica():
+
+		print "Escuchando..."
+		time.sleep(otros[0]))
+		print "Realizado"
+	
+	def cualquiera():
+
+		print "Ejecutando "+name+"..."
+		time.sleep(otros[0]))
+		print "Realizado"
+		
