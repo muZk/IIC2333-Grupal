49f1ebe2774bea6fdefdfec096aa9918ce61b10e
Diego <dgcastil@uc.cl>
2013-04-19 11:42:11 -0400

    actualizado

--- Llamada.py
+++ Llamada.py
@@ -2,13 +2,13 @@ import time
 
 class Llamada(Process):
 	
-	def realizar_llamada():
+	def realizar_llamada(self):
 
 		print "Llamando a "+otros[0]+"..."
 		time.sleep(otros[1]))
 		print "Llamada finalizada"
 
-	def recibir_llamada():
+	def recibir_llamada(self):
 
 		print "Llamada entrante de "+otros[0]+"..."
 		time.sleep(otros[1]))
--- Mensaje.py
+++ Mensaje.py
@@ -2,13 +2,13 @@ import time
 
 class Mensaje(Process):
 	
-	def enviar_mensaje():
+	def enviar_mensaje(self):
 
 		print "Enviando a "+otros[0]+"..."
 		time.sleep(len(otros[1])*20/1000)
 		print "Mensaje enviado"
 
-	def recibir_mensaje():
+	def recibir_mensaje(self):
 		print "Mensaje entrante de "+otros[0]+"..."
 		time.sleep(len(otros[1])*20/1000)
 		print "Mensaje recibido"
