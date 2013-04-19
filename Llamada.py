da93ec7423c0752d38e3b472460ab5a51170ea09
Diego <dgcastil@uc.cl>
2013-04-19 10:24:55 -0400

    clase llamada v1

--- Llamada.py
+++ Llamada.py
@@ -1,3 +1,14 @@
+import time
+
 class Llamada(Process):
-	pass
+	
 	def realizar_llamada():
+
+		print "Llamando a "+otros[0]+"..."
+		time.sleep(otros[1]))
+    	print "Llamada finalizada"
+
+    def recibir_llamada():
+    	print "Llamada entrante de "+otros[0]+"..."
+		time.sleep(otros[1]))
+    	print "Llamada finalizada"
