00514c50f5e457201c46884c3fd3e241e04a85b8
Diego <dgcastil@uc.cl>
2013-04-19 10:41:46 -0400

    Modificado Llamada, Mensaje creado faltan detalles

---
+++ Mensaje.py
@@ -0,0 +1,14 @@
+import time
+
+class Mensaje(Process):
+	
+	def enviar_mensaje():
+
+		print "Enviando a "+otros[0]+"..."
+		time.sleep(len(otros[1]))
+		print "Mensaje enviado"
+
+	def recibir_llamada():
+		print "Mensaje entrante de "+otros[0]+"..."
+		time.sleep(len(otros[1]))
+		print "Mensaje recibido"
