a81e4088985211d3ca06eb793cab7646804c6c21
nggomez <nggomez@uc.cl>
2013-04-22 14:14:37 -0200

    GUI de window a consola

--- GUI.py
+++ GUI.py
@@ -9,43 +9,46 @@ import thread
 
 class GUI:
     def __init__(self,so):
-        self.window = Tkinter.Tk()
+        
         self.so = so
+        print self.opciones()
+        self.mainloop()
         
-        ## Llamadas
-        Tkinter.Label(self.window, text="Numero telefono").pack()
-        self.input_numero = Tkinter.Entry(self.window)
-        self.input_numero.pack()
-    
-        ## Mensajes de texto
-        Tkinter.Label(self.window, text="Mensaje de Texto").pack()
-        self.input_mensaje = Tkinter.Entry(self.window)
-        self.input_mensaje.pack()
-        
-        # Boton para llamar
-        Tkinter.Button(self.window, text="LLamar a numero", command=self.llamar).pack()
-        # Boton para cortar llamada
-        Tkinter.Button(self.window, text="Terminar llamada", command=self.cortar).pack()
-        # Boton para enviar mensaje
-        Tkinter.Button(self.window, text="Enviar mensaje", command=self.enviar_mensaje).pack()
-        # Bot�n para ver contactos
-        Tkinter.Button(self.window, text="Ver contactos", command=self.ver_contactos).pack()
-        # Boton para historial de llamadas
-        Tkinter.Button(self.window, text="Ver historial llamadas", command=self.ver_contactos).pack()
-        # Boton Ver mensajes
-        Tkinter.Button(self.window, text="Ver mensajes", command=self.ver_contactos).pack()
-        # Boton Ver procesos
-        Tkinter.Button(self.window, text="Ver procesos", command=self.ver_contactos).pack()
-        # Boton Ejecutar proceso
-        Tkinter.Button(self.window, text="Ejecutar proceso", command=self.ver_contactos).pack()
-        
-        self.window.mainloop()
+    def opciones(self):
+        return "(1) Llamar\n(2) Mandar mensaje\n(3) Ver contactos\n(4) Revisar historial\n(5) Ver procesos\n(6) Cargar archivo\n(7) Ejecutar proceso\n(8) Ver mensajes\n(9) Agregar contacto\n(0) Salir\n"
+
+    def mainloop(self):
+        run = True
+        while run:
+            command = raw_input() # pedimos el input al usuario  
+            if command == "1":
+                self.llamar()
+            elif command == "2":
+                self.enviar_mensaje()
+            elif command == "3":
+                self.ver_contactos()
+            elif command == "4":
+                self.historial_llamadas()
+            elif command == "5":
+                self.ver_procesos()
+            elif command == "6":
+                pass
+            elif command == "7":
+                self.ejecutar_proceso()
+            elif command == "8":
+                self.ver_mensajes()
+            elif command == "9":
+                self.agregar_contacto()
+            elif command == "0":
+                pass
         
     def getNumero(self): # con este metodo se obtiene el valor del input para el telefono
-        return self.input_numero.get()
+        input_numero = raw_input('Ingrese el número Telefónico al cual desea llamar')
+        return input_numero
     
     def getMensaje(self): # con este metodo se obtiene el valor del input para el mensaje
-        return self.input_mensaje.get()
+        input_mensaje = raw_input('Ingrese el Mensaje de Texto')
+        return input_mensaje
         
     def llamar(self):
         print 'Llamando a {}'.format(self.getNumero())
@@ -60,6 +63,9 @@ class GUI:
         
     def ver_contactos(self):
         print 'Viendo contactos'
+        
+    def agregar_contacto(self):
+        print 'Agregando contacto'
         
     def historial_llamadas(self):
         print 'Historial llamadas'
